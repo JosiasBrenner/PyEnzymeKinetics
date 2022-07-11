4ff1d82967e98adae9e00312b8302e4f22aab47e
haeussma <83341109+haeussma@users.noreply.github.com>
2022-07-11 14:59:52 +0200

    changed demo data

--- pyenzymekinetics/parameterestimator/helper/load_utitlity.py
+++ pyenzymekinetics/parameterestimator/helper/load_utitlity.py
@@ -1,12 +1,10 @@
 import numpy as np
 from pyenzymekinetics.parameterestimator import EnzymeKinetics
 
-concentration_data = np.fromfile("data/concentration")
-#concentration_data = np.reshape(concentration_data,(7,21))/1000
+absorbance_measured = np.fromfile("data/concentration")
+absorbance_measured = np.reshape(absorbance_measured, (7, 21))
 calibration_abso = np.fromfile("data/calibration_abso")
 calibration_conc = np.fromfile("data/calibration_conc")
 
 time_data = np.fromfile("data/time")
 init_substrate = np.array([1, 2.5, 5, 7.5, 10, 20, 30])
-ek = EnzymeKinetics(time_data, product=concentration_data,
-                    init_substrate=init_substrate)
