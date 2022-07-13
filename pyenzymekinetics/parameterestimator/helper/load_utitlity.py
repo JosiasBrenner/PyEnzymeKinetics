2cf577f63ffa1b15870a867796c8ffd15f67ebf0
haeussma <83341109+haeussma@users.noreply.github.com>
2022-07-13 09:02:14 +0200

    added new time array

--- pyenzymekinetics/parameterestimator/helper/load_utitlity.py
+++ pyenzymekinetics/parameterestimator/helper/load_utitlity.py
@@ -5,6 +5,7 @@ absorbance_measured = np.fromfile("data/concentration")
 absorbance_measured = np.reshape(absorbance_measured, (7, 21))
 calibration_abso = np.fromfile("data/calibration_abso")
 calibration_conc = np.fromfile("data/calibration_conc")
+time = np.linspace(0, 20, 21)
 
 time_data = np.fromfile("data/time")
 init_substrate = np.array([1, 2.5, 5, 7.5, 10, 20, 30])
