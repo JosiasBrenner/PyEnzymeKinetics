d43071d3e676c9053d5d7c8e188a8d2e36f70994
haeussma <83341109+haeussma@users.noreply.github.com>
2022-07-14 11:31:36 +0200

    deleted unnecessary import

--- pyenzymekinetics/parameterestimator/__init__.py
+++ pyenzymekinetics/parameterestimator/__init__.py
@@ -1,4 +1,2 @@
 from pyenzymekinetics.parameterestimator.fit import Fit
-from pyenzymekinetics.parameterestimator.models import *
 from pyenzymekinetics.parameterestimator.enzymekinetics import EnzymeKinetics
-
