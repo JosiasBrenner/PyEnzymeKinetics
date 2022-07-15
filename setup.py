79b09c3ed932fb7226d37152230a241edac69511
haeussma <83341109+haeussma@users.noreply.github.com>
2022-07-15 18:24:27 +0200

    changes to build package

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-import setuptools 
+from pkg_resources import parse_requirements
+import setuptools
 
 setuptools.setup(
     name="PyEnzymeKinetics",
-    version="1.0",
+    version="1.1.6",
+    url="https://github.com/haeussma/PyEnzymeKinetics",
     author="Haeussler, Max",
     author_email="max.haeussler@gmail.com",
     packages=setuptools.find_packages(),
-    requires=["pyenzyme"]
+    install_requires=["lmfit", "matplotlib", "scipy", "numpy", ]
 )
